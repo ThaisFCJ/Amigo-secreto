b49a1588be4eb5e33b3f4383dd421a954f0e47de
Thaís Ferreira <thaisf.c.dejesus@gmail.com>
2025-02-16 13:17:52 -0300

    Adição de função para reiniciar o jogo

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function adicionarAmigo(){
     }else{
         amigos.push(inputElement.value); // se valor diferente de vazio, envia para o array (amigos)
         inputElement.value = '' // limpa o input
+        listaVerificada = true; // indica que há um valor na lista
     }
 
    listaTabela() // executando o código a partir do momento que adiciona o amigo na lista
@@ -34,19 +35,19 @@ function listaTabela(){
 function sortearAmigo(){
     let lista = document.getElementById('listaAmigos') 
     lista.innerHTML = ''
-    if (!listaVerificada && lista.innerHTML == ''){  
+    if (!listaVerificada && amigos.length ==0){  // o valor do listaVerificada precisa ser true para o if poder executar a função
          alert("Por favor, insira um nome.");
         
     } else{
         if (amigos.length === 0){
-    
             vazio()
+        } else{
+            let resultado = document.getElementById('resultado') // id resultado do HTML
+            let sortear = Math.floor(Math.random()* amigos.length); // pega a quantidade de elementos de forma aleatória
+            let sorteio = amigos[sortear] // pega os elementos e atribui seus nomes 
+            resultado.innerHTML = sorteio
+            amigos.splice(sortear, 1) // impede que haja repetição
         }
-        let resultado = document.getElementById('resultado') // id resultado do HTML
-        let sortear = Math.floor(Math.random()* amigos.length); // pega a quantidade de elementos de forma aleatória
-        let sorteio = amigos[sortear] // pega os elementos e atribui seus nomes 
-        resultado.innerHTML = sorteio
-        amigos.splice(sortear, 1) // impede que haja repetição
 
     }
 }
@@ -55,5 +56,6 @@ function vazio (){
     alert("A lista acabou! O jogo será reiniciado")
     let resultado = document.getElementById('resultado')
     resultado.innerHTML = ''
+    listaVerificada = false;
 }
 
